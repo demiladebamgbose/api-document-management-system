ba8f574cc5987e9e30088a8070d2a480f9c410cb
demiladebamgbose <demiladebamgbose94@gmail.com>
2016-10-30 11:55:10 +0100

    remove config file

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,7 +3,7 @@
 
   var jwt = require('jsonwebtoken');
   //var dev_secret = require('./../../config/config').secret ;
-  var secret = dev_secret || process.env.secret ;
+  var secret = process.env.secret ;
 
   var Auth = {
 
