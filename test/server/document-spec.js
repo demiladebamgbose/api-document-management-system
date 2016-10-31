1b890361a06096fe8aaad76a53814511977c23ad
demiladebamgbose <demiladebamgbose94@gmail.com>
2016-10-31 13:06:06 +0100

    convert document-spec to ES6

--- test/server/document-spec.js
+++ test/server/document-spec.js
@@ -1,11 +1,11 @@
-var expect = require('chai').expect,
+const expect = require('chai').expect,
   express = require('../../main'),
   supertest = require('supertest'),
   api = supertest(express),
   jwt = require('jsonwebtoken'),
   secret =  process.env.secret;
 
-var token = jwt.sign({
+const token = jwt.sign({
   emailaddress: '123@abc.com',
   password:'12345',
   RoleId: 1,
@@ -14,11 +14,10 @@ var token = jwt.sign({
   expiresIn: 60*60*24
 });
 
-console.log(token);
+describe('Document', () => {
+  'use strict';
 
-describe('Document', function () {
-
-  it('should create a new document with a defined published date', function (done) {
+  it('should create a new document with a defined published date', (done) => {
     api.post('/api/documents')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
@@ -27,7 +26,7 @@ describe('Document', function () {
       content: 'Test Content for testing sake',
       RoleId: 3
     })
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(200);
       expect(res.body.title).to.be.equal('Test Document');
       expect(res.body).have.property('createdAt');
@@ -35,7 +34,7 @@ describe('Document', function () {
     });
   });
 
-  it('should not create a document with incomplete details', function (done) {
+  it('should not create a document with incomplete details', (done) => {
     api.post('/api/documents')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
@@ -44,7 +43,7 @@ describe('Document', function () {
       content: '',
       RoleId: 3
     })
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(401);
       expect(res.body).to.have.property('message');
       expect(res.body.message).to.be.equal('Feilds cannot be empty');
@@ -52,7 +51,7 @@ describe('Document', function () {
     });
   });
 
-  it('should only create a document with a unique title', function (done) {
+  it('should only create a document with a unique title', (done) => {
     api.post('/api/documents')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
@@ -61,7 +60,7 @@ describe('Document', function () {
       content: 'Test Content for testing sake',
       RoleId: 3
     })
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(422);
       expect(res.body).to.have.property('message');
       expect(res.body.message).to.be.equal('Title already exists');
@@ -69,7 +68,7 @@ describe('Document', function () {
     });
   });
 
-  it('should only create a document with a valid role', function (done) {
+  it('should only create a document with a valid role', (done) => {
     api.post('/api/documents')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
@@ -78,7 +77,7 @@ describe('Document', function () {
       content: 'Test Content for testing sake',
       RoleId: 7
     })
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(422);
       expect(res.body).to.have.property('message');
       expect(res.body.message).to.be.equal('Role does not exist');
@@ -86,10 +85,10 @@ describe('Document', function () {
     });
   });
 
-  it('Every Document should have a unique title', function (done) {
+  it('Every Document should have a unique title', (done) => {
     function checkUniqueTitle (rolesArray) {
-      var titles = [];
-      for (var i = 0; i < rolesArray.length; i++) {
+      let titles = [];
+      for (let i = 0; i < rolesArray.length; i++) {
         expect(titles.indexOf(rolesArray[i].title)).to.equal(-1);
         titles.push(rolesArray[i].title);
       }
@@ -98,7 +97,7 @@ describe('Document', function () {
     api.get('/api/documents?limit=20')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(200);
       expect(res.body.length).to.be.at.most(20);
       checkUniqueTitle(res.body);
@@ -106,11 +105,11 @@ describe('Document', function () {
     });
   });
 
-  it('should retrieve a document from the database', function (done) {
+  it('should retrieve a document from the database',(done) => {
     api.get('/api/documents/4')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(200);
       expect(typeof(res.body)).to.be.equal('object');
       expect(res.body.title).to.be.equal('Yellow document');
@@ -118,11 +117,11 @@ describe('Document', function () {
     });
   });
 
-  it('should return limited documents with a set query limit', function (done) {
+  it('should return limited documents with a set query limit',(done) => {
     api.get('/api/documents?limit=4')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(200);
       expect(res.body.length).to.be.equal(4);
       expect(res.body.length).to.be.at.most(4);
@@ -131,11 +130,11 @@ describe('Document', function () {
   });
 
   it('should return limited documents with a set query limit and offset',
-  function (done) {
+  (done) => {
     api.get('/api/documents?limit=4&page=2')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(200);
       expect(res.body.length).to.be.equal(4);
       done();
@@ -143,11 +142,11 @@ describe('Document', function () {
   });
 
   it('should return documents starting with the most recently created',
-  function (done) {
+  (done) => {
     api.get('/api/documents?limit=4&page=1')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(200);
       expect(res.body.length).to.be.equal(4);
       expect(res.body[0].title).to.be.equal('Test Document');
@@ -155,14 +154,14 @@ describe('Document', function () {
     });
   });
 
-  it('should update attributes of a document', function (done) {
+  it('should update attributes of a document', (done) => {
     api.put('/api/documents/1')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
     .send({
       title: 'A new title'
     })
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(200);
       expect(typeof(res.body)).to.be.equal('object');
       expect(res.body.title).to.be.equal('A new title');
@@ -170,11 +169,11 @@ describe('Document', function () {
     });
   });
 
-  it('should delete a document from the database', function (done) {
+  it('should delete a document from the database', (done) => {
     api.delete('/api/documents/1')
     .set('x-access-token', token)
     .set('Accept', 'application/json')
-    .end(function (err, res) {
+    .end((err, res) => {
       expect(res.status).to.be.equal(200);
       expect(res.body).to.be.equal(1);
       done();
